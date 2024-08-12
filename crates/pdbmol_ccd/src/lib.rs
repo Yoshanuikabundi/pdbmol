adfa703e1fcb553475242a57b313358f1fd0cbb9
Josh Mitchell <yoshanuikabundi@gmail.com>
2024-08-12 22:08:01 +1000

    Provide CCD data from pdbmol_ccd

--- crates/pdbmol_ccd/src/lib.rs
+++ crates/pdbmol_ccd/src/lib.rs
@@ -1,1 +1,8 @@
+use std::{collections::HashMap, sync::LazyLock};
+
 pub use pdbmol_ccdparse::datatypes;
+
+pub static CCD: LazyLock<HashMap<&str, datatypes::Residue>> = LazyLock::new(|| {
+    let cif_str = include_str!("../../../data/ccd-20240406.cif");
+    pdbmol_ccdparse::parse_ccd(cif_str).unwrap()
+});
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,10 @@
-use pdbmol_ccd::datatypes::Residue;
-use pdbmol_cif as cif;
-use std::{collections::HashMap, env, error::Error, fs, path::Path};
+use pdbmol_ccd::CCD;
+use std::env;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 2 {
-        println!("{:?}", args);
-        let cif_path = Path::new(&args[1]);
-        let cif_file = fs::read_to_string(cif_path)?;
-        let cif_parsed = cif::parse(&cif_file);
-
-        match cif_parsed {
-            Ok(d) => {
-                let res = Residue::try_from(&d.into_iter().next().ok_or("no datablock")?.1);
-                println!("{:#?}", res);
-            }
-            Err(e) => println!("{}", e.to_string()),
-        }
-    } else {
-        let cif_path = Path::new("data/ccd-20240406.cif");
-        let cif_file = fs::read_to_string(cif_path)?;
-        let cif_parsed = cif::parse(&cif_file);
-
-        match cif_parsed {
-            Err(e) => println!("{}", e.to_string()),
-            Ok(d) => {
-                let map: Result<HashMap<&'_ str, Residue>, _> = d
-                    .iter()
-                    .filter(|(key, _)| !["UNL"].contains(key)) // Filter out special residues
-                    .map(|(key, value)| {
-                        println!("loading residue {key}");
-                        Residue::try_from(value).map(|v| (*key, v))
-                    })
-                    .collect();
-                println!("{:#?}", map?)
-            }
-        };
+    for resname in &args[1..] {
+        println!("{:#?}", CCD[resname.as_str()]);
     }
-
-    Ok(())
 }
